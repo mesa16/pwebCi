438f7d66e039eb2c43d63a32bb6e873fbc2d6534
mesa16 <32347244+mesa16@users.noreply.github.com>
2018-01-11 17:32:27 -0800

    pwebCi

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,45 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$this->load->view("layout/header");
-		 $this->load->view("layout/nav");
-		$this->load->view("index");
-		$this->load->view("layout/footer");
+		$this->load->template("index");
 
 	}
 
 	public function pelangi()
 	{
-		$this->load->view("layout/header");
-	  $this->load->view("layout/nav");
-	  $this->load->view("pelangi");
-	  $this->load->view("layout/footer");
+	  $this->load->template("pelangi");
 	}
 
 	public function cicak()
 	{
-		$this->load->view("layout/header");
-	  $this->load->view("layout/nav");
-	  $this->load->view("cicak");
-	  $this->load->view("layout/footer");
+	  $this->load->template("cicak");
 	}
 	public function naikkereta(){
-		$this->load->view("layout/header");
-	  $this->load->view("layout/nav");
-	  $this->load->view("naikkereta");
-	  $this->load->view("layout/footer");
+	  $this->load->template("naikkereta");
 	}
 	public function balonku(){
-		$this->load->view("layout/header");
-	  $this->load->view("layout/nav");
-	  $this->load->view("Balonku");
-	  $this->load->view("layout/footer");
+	  $this->load->template("Balonku");
 	}
 	public function naikgunung(){
-		$this->load->view("layout/header");
-	  $this->load->view("layout/nav");
-	  $this->load->view("naikgunung");
-	  $this->load->view("layout/footer");
+	  $this->load->template("naikgunung");
 	}
 
 }
